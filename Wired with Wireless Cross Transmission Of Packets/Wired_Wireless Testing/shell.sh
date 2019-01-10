7f1793a2fbccd78b8850581af2935d6891761a1b
Mahim1997 <1505022.mm@ugrad.cse.buet.ac.bd>
2019-01-11 03:56:43 +0600

    Sat awk file likha hoise

--- Wired with Wireless Cross Transmission Of Packets/Wired_Wireless Testing/shell.sh
+++ Wired with Wireless Cross Transmission Of Packets/Wired_Wireless Testing/shell.sh
@@ -1,12 +1,24 @@
 
 clear;
 traceFile="wired-and-wireless.tr";
-ns tclFile.tcl
+
+awkWireless="awkFile.awk";
+awkWired="wiredAwk.awk";
+tclFile="tclFile.tcl";
+
+echo "Running tcl file now for Wired-Wireless Cross Transmission";
+ns "$tclFile" ;
 
 echo ;
-echo "Running awk file now .";
+echo "Running awk file now for wireless nodes .";
+awk -f "$awkWireless" "$traceFile";
 
-awk -f awkFile.awk "$traceFile";
+
+echo ;
+
+echo "Now Running awk file for WIRED nodes...";
+
+awk -f "$awkWired" "$traceFile"
 
 echo ;
 echo "Running nam file now .";
